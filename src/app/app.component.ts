8c5223918e4a262237f7e6819c2abecaf6dec73d
DeN2828 <den-tu@ua.fm>
2023-06-07 22:32:18 +0300

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { SharedService } from './shared.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +11,34 @@ import { SharedService } from './shared.service';
 })
 export class AppComponent {
   title = 'routing-app';
-  public reg : string = "Register";
-  constructor(public sharedService: SharedService) { 
-    
+  isAuthorized: boolean = false;
+
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+
+  constructor(private router: Router, private sharedService: SharedService) {}
+
+  ngOnInit() {
+    this.router.events.pipe(takeUntil(this.ngUnsubscribe)).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkAuthorization();
+      }
+    });
+
+    this.checkAuthorization();
   }
-  
-  
-  
+
+  checkAuthorization() {
+    this.sharedService.access$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((access) => {
+      this.isAuthorized = access;
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  onAuthorizationChange(authorized: boolean) {
+    this.sharedService.access = authorized;
+  }
 }
